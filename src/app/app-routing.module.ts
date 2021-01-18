e527a69611ee78a9f1c8331e31da98e2f27bd740
Felipe Maldonado <felipe.maldonado@sistemasexpertos.cl>
2021-01-18 17:17:04 -0300

    Nuevos componentes generales y clasificación de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { AuthGuard } from 'src/app/helpers/auth.guard';
+import { LoginComponent } from 'src/app/components/auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  // Everything else
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
